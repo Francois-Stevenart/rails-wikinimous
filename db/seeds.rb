c6f94dc85b9792c1afe59ebd411597e91a58f054
Francois-Stevenart <f.stevenart@gmail.com>
2019-08-06 18:43:47 +0200

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+30.times do
+  Article.create(
+    title: Faker::Hipster.sentence,
+    content: Faker::Hipster.paragraph)
+end
